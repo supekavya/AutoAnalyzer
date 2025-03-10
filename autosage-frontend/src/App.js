b62df7a6df599f59db191482cdb3febe19873a17
supekavya <supekavya@gmail.com>
2025-03-10 11:23:26 +0530

    first commit of files

---
+++ autosage-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Suggestions from './components/Suggestions';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Suggestions />
+           
+        </div>
+    );
+};
+
+export default App;
